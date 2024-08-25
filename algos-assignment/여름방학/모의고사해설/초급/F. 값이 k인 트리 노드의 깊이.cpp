da493de0e0c83334c2d8f090ec20496b25a1e59f
최연재 <80452199+yeonjae02@users.noreply.github.com>
2024-08-26 08:55:32 +0900

    feat: 초급모의고사 E번 (25511) 코드 업로드

---
+++ algos-assignment/여름방학/모의고사해설/초급/F. 값이 k인 트리 노드의 깊이.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// BOJ 25511
+
+#define FASTIO ios::sync_with_stdio(false); cin.tie(NULL);
+#define MAX 100001
+
+vector<int> v[MAX];
+vector<int> arr(MAX);
+bool visit[MAX];
+int n, k, ans;
+
+void dfs(int now, int d) {
+	if (visit[now]) return;
+	visit[now] = true;
+
+	if (arr[now] == k) {
+		cout << d;
+		exit(0);
+	}
+
+	for (int i = 0; i < v[now].size(); i++)
+		dfs(v[now][i], d + 1);
+}
+
+int main() {
+	FASTIO;
+
+	cin >> n >> k;
+
+	for (int i = 0, a, b; i < n - 1; i++) {
+		cin >> a >> b;
+		v[a].push_back(b);
+	}
+
+	for (int i = 0; i < n; i++)cin >> arr[i];
+	dfs(0, 0);
+	return 0;
+}
