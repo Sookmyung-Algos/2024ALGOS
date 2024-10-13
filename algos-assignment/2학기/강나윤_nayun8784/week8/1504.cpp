9e1d88b9685c49eca682cc21c0a16e49a9565e39
nayunn <104406100+nayunn@users.noreply.github.com>
2024-10-13 21:38:34 +0900

    Update 1504.cpp

--- algos-assignment/2학기/강나윤_nayun8784/week8/1504.cpp
+++ algos-assignment/2학기/강나윤_nayun8784/week8/1504.cpp
@@ -1,67 +1,59 @@
-#include <stdio.h>
+#include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
 #define INF 987654321
 using namespace std;
 
-int n,e,v1,v2;
-int a,b,c;
-vector<pair<int,int> >node[805];
-int value[805];
-priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > >pq;
+int n, d[801];
+vector<pair<int, int>> node[801];
 
-void Dij(int k){
-	for(int i=1;i<=n;i++)
-		value[i] = INF;
-	
-	value[k] = 0;
-	
-	pq.push(make_pair(0,k));
-	
-	while(!pq.empty()){
-		int X = pq.top().first;
-		int U = pq.top().second;
+int dijkstra(int x, int y) {
+	for (int i = 1; i <= n; i++)
+		d[i] = INF;
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	pq.push({ 0, x });
+	d[x] = 0;
+	while (!pq.empty()) {
+		int dist = pq.top().first;
+		int curr = pq.top().second;
 		pq.pop();
-		
-		for(int i=0;i<node[U].size();i++){
-			int V = node[U][i].first;
-			int W = node[U][i].second;
-			if(X+W<value[V]){
-				value[V] = X+W;
-				pq.push(make_pair(value[V],V));
+		for (int i = 0; i < node[curr].size(); i++) {
+			int ndist = node[curr][i].first;
+			int ncurr = node[curr][i].second;
+			if (d[ncurr] > dist + ndist) {
+				d[ncurr] = dist + ndist;
+				pq.push({ d[ncurr], ncurr });
 			}
 		}
 	}
+	return d[y];
 }
 
-int main(){	
-	scanf("%d %d",&n,&e);
-	
-	for(int i=0;i<e;i++){
-		scanf("%d %d %d",&a,&b,&c);
-		node[a].push_back(make_pair(b,c));
-		node[b].push_back(make_pair(a,c));
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	int e, v1, v2, dist = 0, ans1 = 987654321, ans2 = 987654321;
+	cin >> n >> e;
+	for (int i = 0; i < e; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		node[a].push_back({ c, b });
+		node[b].push_back({ c, a });
 	}
+	cin >> v1 >> v2;
 	
-	scanf("%d %d",&v1,&v2);
+	if (dijkstra(1, v1) == INF || dijkstra(v1, v2) == INF || dijkstra(v2, n) == INF)
+		ans1 = INF;
+	else
+		ans1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, n);
+
+	if (dijkstra(1, v2) == INF || dijkstra(v2, v1) == INF || dijkstra(v1, n) == INF)
+		ans2 = INF;
+	else
+		ans2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, n);
 	
-	long long int v1_1,v1_v2,v1_n,v2_1,v2_n;
-	
-	Dij(v1);
-	v1_1 = value[1];
-	v1_v2 = value[v2];
-	v1_n = value[n];
-	Dij(v2);
-	v2_1 = value[1];
-	v2_n = value[n];
-	
-	long long int ans1 = v1_1 + v1_v2 + v2_n;
-	long long int ans2 = v2_1 + v1_v2 + v1_n;
-	long long int ans = ans1 < ans2 ? ans1 : ans2;
-	
-	if(ans>=INF) ans = -1;
-	
-	printf("%lld",ans);
-	
-	return 0;
+	if (ans1 == INF && ans2 == INF)
+		cout << "-1";
+	else
+		cout << min(ans1, ans2);
 }
